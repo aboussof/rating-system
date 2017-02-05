530defca6c0de699c4db2f1eae9ac0b350ada218
aboussof <lover-and-friend@hotmail.com>
2017-02-05 11:39:05 +0000

    Create rating.js

---
+++ js/rating.js
@@ -0,0 +1,60 @@
+
+clicked = false;
+
+$(".rating").mouseover(function () {
+    $(this).css('cursor', 'pointer');
+    if (!clicked) {
+        var id = $(this).attr('id');
+        var id_splited = id.split('-')
+        var index = id_splited[1];
+        for (i = index; i >= 0; i--)
+            $('#rat-' + i).removeClass('glyphicon-star-empty').addClass("glyphicon-star");
+    }
+});
+
+$(".rating").mouseleave(function () {
+    if (!clicked) {
+        var id = $(this).attr('id');
+        var id_splited = id.split('-');
+        var value = id_splited[1];
+
+        $(this).removeClass('glyphicon-star').addClass("glyphicon-star-empty");
+
+        var val = parseInt(value) + 1;
+
+        for (m = val; m <= 5; m++)
+            $('#rat-' + m).removeClass('glyphicon-star').addClass("glyphicon-star-empty");
+
+    }
+});
+
+$("#ratin-div").mouseleave(function () {
+    if (!clicked) {
+        for (i = 1; i <= 5; i++)
+            $('#rat-' + i).removeClass('glyphicon-star').addClass("glyphicon-star-empty");
+    }
+});
+
+
+$(".rating").click(function () {
+    clicked = true
+
+    $("#ratin-div").css('border', '');
+    $("#id_rat").css('display', 'none');
+
+    var id = $(this).attr('id');
+    var id_splited = id.split('-')
+    var value = id_splited[1];
+
+    $("#rat-value").val(value);
+
+    for (i = 1; i <= value; i++)
+        $('#rat-' + i).removeClass('glyphicon-star-empty').addClass("glyphicon-star");
+
+    var val = parseInt(value) + 1;
+
+    for (m = val; m <= 5; m++)
+       $('#rat-' + m).removeClass('glyphicon-star').addClass("glyphicon-star-empty");
+});
+
+
